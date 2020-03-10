a438ca7901da98c0eb93e16c22db8af42324ccea
junseongday <junseongday@gmail.com>
2020-03-11 02:06:14 +0900

    10장완

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,18 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map(todo => {
-        return <TodoListItem todo={todo} id={todo.id} />;
+        return (
+          <TodoListItem
+            todo={todo}
+            key={todo.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        );
       })}
     </div>
   );
